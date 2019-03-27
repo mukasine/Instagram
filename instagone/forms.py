1500bf4bdf12ca1695b89201f78c9f874737480f
mukasine marie claire <mclaremukasine@gmail.com>
2019-03-27 16:01:18 +0200

    adding class NewImageForm by form.py

--- instagone/forms.py
+++ instagone/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from .models import Image,Profile
 
-
+class instagoneLetterForm(forms.Form):
+    your_name = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')
 class NewImageForm(forms.ModelForm):
     class Meta:
         model = Image
